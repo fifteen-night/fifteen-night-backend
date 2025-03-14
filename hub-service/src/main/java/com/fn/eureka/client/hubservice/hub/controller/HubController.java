619a33e46c98f7a22fead21485cc6bdbab1a2098
Goldbar97 <ehrm1357@gmail.com>
2025-03-14 12:52:53 +0900

    #5 feat: hub - 초기 설정

---
+++ hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/controller/HubController.java
@@ -0,0 +1,11 @@
+package com.fn.eureka.client.hubservice.hub.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class HubController {
+
+}
