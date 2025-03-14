3530dbb019b4a476788c349e98d2239e8cb92bd4
hanjihae <wlgo32@naver.com>
2025-03-14 17:22:37 +0900

    #4 feat: common - 공통 모듈 Exception, BaseEntity 추가

---
+++ common/src/main/java/com/fn/global/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.fn.global.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
