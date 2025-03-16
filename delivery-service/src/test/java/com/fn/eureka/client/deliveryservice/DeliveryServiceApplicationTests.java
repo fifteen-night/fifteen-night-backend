700b854fd1f5079a5e235ee47b52125e006b98a7
김재현 <94097685+iconew123@users.noreply.github.com>
2025-03-16 23:51:10 +0900

    #9 feat: delivery-service 모듈 생성
    - delivery-service 의 유레카 클라이언트 모듈 생성
    - ORM, DB, SWAGGER 의존성 추가

---
+++ delivery-service/src/test/java/com/fn/eureka/client/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fn.eureka.client.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
