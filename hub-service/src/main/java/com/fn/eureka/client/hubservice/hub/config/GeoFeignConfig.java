c2d22ec936ea3870fe5098a07a7c7ba78cce74a6
Goldbar97 <ehrm1357@gmail.com>
2025-03-17 19:12:42 +0900

    #5 feat: hub - 네이버 API 연동

---
+++ hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/config/GeoFeignConfig.java
@@ -0,0 +1,24 @@
+package com.fn.eureka.client.hubservice.hub.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.RequestInterceptor;
+
+@Configuration
+public class GeoFeignConfig {
+
+	@Value("${naver.api.client-id}")
+	private String clientId;
+	@Value("${naver.api.client-secret}")
+	private String clientSecret;
+
+	@Bean
+	public RequestInterceptor requestInterceptor() {
+		return requestTemplate -> {
+			requestTemplate.header("X-NCP-APIGW-API-KEY-ID", clientId);
+			requestTemplate.header("X-NCP-APIGW-API-KEY", clientSecret);
+		};
+	}
+}
---
+++ hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/infrastructure/GeoFeignClient.java
@@ -0,0 +1,19 @@
+package com.fn.eureka.client.hubservice.hub.infrastructure;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fn.eureka.client.hubservice.hub.application.dto.GeoResponse;
+import com.fn.eureka.client.hubservice.hub.config.GeoFeignConfig;
+
+@FeignClient(
+	name = "geoClient",
+	url = "https://naveropenapi.apigw.ntruss.com",
+	configuration = GeoFeignConfig.class
+)
+public interface GeoFeignClient {
+
+	@GetMapping("/map-geocode/v2/geocode")
+	GeoResponse getPoint(@RequestParam("query") String query);
+}
