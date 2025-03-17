01dba430bc7c62b346dd76624e362793ea359a48
김재현 <94097685+iconew123@users.noreply.github.com>
2025-03-18 01:21:27 +0900

    #30 fix: delivery-service Common모듈 수정
    - CustomApiException 생성해서 api에서 발생할 수 있는 다양한 예외처리
    - 추가 메소드 사용해서 구현 가능

---
+++ common/src/main/java/com/fn/common/global/exception/CustomApiException.java
@@ -0,0 +1,28 @@
+package com.fn.common.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.fn.common.global.exception.type.BaseException;
+import com.fn.common.global.exception.type.ExceptionType;
+
+public class CustomApiException extends RuntimeException {
+
+	// 커스텀 api Exception
+	// API에서 발생할 수 있는 다양한 예외를 일관되게 처리
+
+	private final HttpStatus httpStatus;
+	private ExceptionType exceptionType;
+
+
+	public CustomApiException(ExceptionType exceptionType){
+		super(exceptionType.getMessage());
+		this.exceptionType = exceptionType;
+		this.httpStatus = exceptionType.getHttpStatus();
+	}
+
+	public CustomApiException(String message, HttpStatus httpStatus) {
+		super(message);
+		this.exceptionType = BaseException.SERVER_ERROR;
+		this.httpStatus = httpStatus;
+	}
+}
