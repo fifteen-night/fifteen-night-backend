f9b3e53e7f799afbb7719ed81d110d4ae391f1a6
Goldbar97 <ehrm1357@gmail.com>
2025-03-17 19:12:42 +0900

    #5 feat: hub - 네이버 API 연동

---
+++ hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/application/dto/GeoResponse.java
@@ -0,0 +1,18 @@
+package com.fn.eureka.client.hubservice.hub.application.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class GeoResponse {
+	private List<Address> addresses;
+
+	@Getter
+	public static class Address {
+		private String roadAddress;
+		private String jibunAddress;
+		private String x; // longitude
+		private String y; // latitude
+	}
+}
