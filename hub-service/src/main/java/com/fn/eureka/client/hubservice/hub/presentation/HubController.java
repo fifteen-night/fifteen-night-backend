ea257351e4a5fb319354741af335fb756877398b
Goldbar97 <ehrm1357@gmail.com>
2025-03-18 16:38:17 +0900

    #5 feat: hub - 수정, 삭제 기능 추가

--- hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/presentation/HubController.java
+++ hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/presentation/HubController.java
@@ -3,8 +3,11 @@ package com.fn.eureka.client.hubservice.hub.presentation;
 import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fn.eureka.client.hubservice.hub.application.HubService;
 import com.fn.eureka.client.hubservice.hub.application.dto.request.CreateHubRequest;
+import com.fn.eureka.client.hubservice.hub.application.dto.request.UpdateHubRequest;
 import com.fn.eureka.client.hubservice.hub.application.dto.response.CreateHubResponse;
 import com.fn.eureka.client.hubservice.hub.application.dto.response.ReadHubResponse;
 import com.fn.eureka.client.hubservice.hub.application.dto.response.SearchHubResponse;
@@ -49,5 +53,21 @@ public class HubController {
 		SearchHubResponse response = hubService.searchHub(pageable, hubName);
 
 		return ResponseEntity.ok(response);
+	}
+
+	@PatchMapping("/{hubId}")
+	public ResponseEntity<Void> updateHub(@PathVariable("hubId") UUID hubId, @RequestBody UpdateHubRequest request) {
+
+		hubService.updateHub(hubId, request);
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+
+	@DeleteMapping("/{hubId}")
+	public ResponseEntity<Void> deleteHub(@PathVariable("hubId") UUID hubId) {
+
+		hubService.deleteHub(hubId);
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
