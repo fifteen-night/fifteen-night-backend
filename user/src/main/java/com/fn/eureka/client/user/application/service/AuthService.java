fc33da7e5e1c7c4fb2f189cb5cbe502c5480e3ea
STW5 <xodnd8384@gmail.com>
2025-03-18 14:45:41 +0900

    #35 refactor: user - 응답 객체 수정
    - 기존 ApiResponseDto 제거하고 CommonResponse 적용

--- user/src/main/java/com/fn/eureka/client/user/application/service/AuthService.java
+++ user/src/main/java/com/fn/eureka/client/user/application/service/AuthService.java
@@ -4,6 +4,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fn.common.global.dto.CommonResponse;
+import com.fn.common.global.success.SuccessCode;
 import com.fn.eureka.client.user.application.dto.auth.request.UserSignInRequestDto;
 import com.fn.eureka.client.user.application.dto.auth.request.UserSignUpRequestDto;
 import com.fn.eureka.client.user.application.dto.auth.response.UserSignInResponseDto;
@@ -24,12 +26,11 @@ public class AuthService {
 	private final JwtUtil jwtUtil;
 
 	@Transactional
-	public UserSignUpResponseDto signUp(UserSignUpRequestDto requestDto) {
+	public CommonResponse<UserSignUpResponseDto> signUp(UserSignUpRequestDto requestDto) {
 		// 이메일 중복 확인
 		if (userRepository.findByUserEmail(requestDto.getUserEmail()).isPresent()) {
 			throw new RuntimeException(AuthException.DUPLICATED_EMAIL.getMessage());
 		}
-
 		// 닉네임 중복 확인
 		if (userRepository.findByUserNickname(requestDto.getUserNickname()).isPresent()) {
 			throw new RuntimeException(AuthException.DUPLICATED_NICKNAME.getMessage());
@@ -49,18 +50,20 @@ public class AuthService {
 
 		userRepository.save(user);
 
-		// 단순 DTO 반환
-		return UserSignUpResponseDto.builder()
+		// DTO 반환
+		UserSignUpResponseDto responseDto = UserSignUpResponseDto.builder()
 			.userId(user.getUserId())
 			.userName(user.getUserName())
 			.userNickname(user.getUserNickname())
 			.userEmail(user.getUserEmail())
 			.userRole(user.getUserRole())
 			.build();
+
+		return new CommonResponse<>(SuccessCode.AUTH_SIGNUP, responseDto);
 	}
 
 	@Transactional(readOnly = true)
-	public UserSignInResponseDto signIn(UserSignInRequestDto requestDto) {
+	public CommonResponse<UserSignInResponseDto> signIn(UserSignInRequestDto requestDto) {
 		// 유저네임으로 유저 조회 후, 비밀번호 검증까지 한 번에 처리
 		User user = userRepository.findByUserName(requestDto.getUserName())
 			.filter(u -> passwordEncoder.matches(requestDto.getUserPassword(), u.getUserPassword()))
@@ -70,11 +73,12 @@ public class AuthService {
 		String accessToken = jwtUtil.createAccessToken(user.getUserId(), user.getUserName(), user.getUserRole());
 		String refreshToken = jwtUtil.createRefreshToken(user.getUserId());
 
-		// 로그인 성공 응답 DTO 반환
-		return UserSignInResponseDto.builder()
+		// DTO 반환
+		UserSignInResponseDto responseDto = UserSignInResponseDto.builder()
 			.accessToken(accessToken)
 			.refreshToken(refreshToken)
 			.build();
-	}
 
+		return new CommonResponse<>(SuccessCode.AUTH_LOGIN, responseDto);
+	}
 }
