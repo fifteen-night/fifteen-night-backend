157e64f9739b0bbcb362146617c15bd36638d46e
STW5 <xodnd8384@gmail.com>
2025-03-18 14:22:52 +0900

    #35 refactor: user - 에러 처리 수정
    - BaseException을 상속받는 UserException, AuthException, JwtException 적용
    - 예외 메시지, 상태 코드, 에러 코드 관리를 Enum으로 통합

--- user/src/main/java/com/fn/eureka/client/user/application/exception/CustomJwtException.java
+++
@@ -1,7 +0,0 @@
-package com.fn.eureka.client.user.application.exception;
-
-public class CustomJwtException extends RuntimeException {
-	public CustomJwtException(String message) {
-		super(message);
-	}
-}
