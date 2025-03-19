e867ca4370f49977f7371106acde6524c7450818
hanjihae <wlgo32@naver.com>
2025-03-19 11:51:31 +0900

    #41 feat: product - 상품 수정

---
+++ company-service/src/test/java/com/fn/eureka/client/companyservice/CompanyServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.fn.eureka.client.companyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyServiceApplicationTests {
+
+}
---
+++ product-service/src/test/java/com/fn/eureka/client/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package com.fn.eureka.client.productservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+}
