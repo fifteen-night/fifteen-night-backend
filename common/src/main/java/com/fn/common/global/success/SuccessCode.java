34b2aceda8e2589c4f66918ea474e306703d670e
Goldbar97 <ehrm1357@gmail.com>
2025-03-19 20:48:56 +0900

    #42 refactor: hub-stock - 불필요한 코드 정리

--- common/src/main/java/com/fn/common/global/success/SuccessCode.java
+++ common/src/main/java/com/fn/common/global/success/SuccessCode.java
@@ -6,20 +6,21 @@ import lombok.Getter;
 
 @Getter
 public enum SuccessCode {
-	
+
 	// 여기서 공통 성공 응답 생성
-  // HubToHub 관련 성공응답
+	// HubToHub 관련 성공응답
 	HUBTOHUB_CREATE(HttpStatus.CREATED, "루트가 성공적으로 생성되었습니다.", "S_HUBTOHUB_CREATE"),
-	HUBTOHUB_SEARCH_ALL(HttpStatus.OK, "모든 루트가 성공적으로 조회되었습니다.","S_HUBTOHUB_SEARCH_ALL" ),
+	HUBTOHUB_SEARCH_ALL(HttpStatus.OK, "모든 루트가 성공적으로 조회되었습니다.", "S_HUBTOHUB_SEARCH_ALL"),
 	HUBTOHUB_SEARCH_ONE(HttpStatus.OK, "단건 루트가 성공적으로 조회되었습니다.", "S_HUBTOHUB_SEARCH_ONE"),
-	HUBTOHUB_SOFT_DELETE(HttpStatus.NO_CONTENT, "해당 루트가 삭제되었습니다.", "S_HUBTOHUB_SOFT_DELETE" ),
-  
-  // Hub 관련 성공응답
+	HUBTOHUB_SOFT_DELETE(HttpStatus.NO_CONTENT, "해당 루트가 삭제되었습니다.", "S_HUBTOHUB_SOFT_DELETE"),
+
+	// Hub 관련 성공응답
 	HUB_CREATE(HttpStatus.CREATED, "허브가 성공적으로 생성되었습니다.", "S_HUB_CREATE"),
 	HUB_SEARCH(HttpStatus.OK, "허브가 성공적으로 조회되었습니다.", "S_HUB_SEARCH"),
 
 	// HubStock 관련 성공 응답
 	HUB_STOCK_CREATE(HttpStatus.CREATED, "허브 재고가 성공적으로 생성되었습니다.", "S_HUB_STOCK_CREATE"),
+	HUB_STOCK_SEARCH(HttpStatus.OK, "허브 재고가 성공적으로 조회되었습니다.", "S_HUB_STOCK_SEARCH"),
 
 	// Auth 관련 성공 응답
 	AUTH_SIGNUP(HttpStatus.CREATED, "회원가입이 성공적으로 완료되었습니다.", "S_AUTH_SIGNUP"),
@@ -29,8 +30,7 @@ public enum SuccessCode {
 	USER_FOUND(HttpStatus.OK, "사용자 정보가 성공적으로 조회되었습니다.", "S_USER_FOUND"),
 	USER_LIST_FOUND(HttpStatus.OK, "사용자 목록이 성공적으로 조회되었습니다.", "S_USER_LIST_FOUND"),
 	USER_UPDATED(HttpStatus.OK, "사용자 정보가 성공적으로 수정되었습니다.", "S_USER_UPDATED"),
-	USER_DELETED(HttpStatus.OK, "사용자 정보가 성공적으로 삭제되었습니다.", "S_USER_DELETED")
-	;
+	USER_DELETED(HttpStatus.OK, "사용자 정보가 성공적으로 삭제되었습니다.", "S_USER_DELETED");
 
 	private final HttpStatus statusCode;
 	private final String message;
