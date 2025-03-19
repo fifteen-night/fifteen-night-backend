8a2bedc8df492ea6a7ffd03ffb50c5b57084c8d9
Goldbar97 <ehrm1357@gmail.com>
2025-03-19 20:48:56 +0900

    #42 refactor: hub-stock - 불필요한 코드 정리

--- hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/infrastructure/HubServiceImpl.java
+++ hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/infrastructure/HubServiceImpl.java
@@ -85,16 +85,4 @@ public class HubServiceImpl implements HubService {
 
 		return hubRepository.findById(id).orElseThrow(() -> new NotFoundException("없는 허브입니다."));
 	}
-
-	@Override
-	public Hub findHubById(UUID id) {
-
-		return hubRepository.findById(id).orElseThrow(() -> new NotFoundException("없는 허브입니다."));
-	}
-
-	@Override
-	public Hub findHubById(UUID id) {
-
-		return hubRepository.findById(id).orElseThrow(() -> new NotFoundException("없는 허브입니다."));
-	}
 }
