0e0b71c165b7f8d6211e3170c0bbbc9bd8b1b4a1
Goldbar97 <ehrm1357@gmail.com>
2025-03-19 11:29:28 +0900

    #5 feat: hub - SuccessCode 추가

--- common/src/main/java/com/fn/common/global/success/SuccessCode.java
+++ common/src/main/java/com/fn/common/global/success/SuccessCode.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 
 @Getter
 public enum SuccessCode {
-	
+
 	// 여기서 공통 성공 응답 생성
 
 	HUBTOHUB_CREATE(HttpStatus.CREATED, "루트가 성공적으로 생성되었습니다.", "S_HUBTOHUB_CREATE"),
-	HUBTOHUB_SEARCH_ONE(HttpStatus.OK, "단건 루트가 성공적으로 조회되었습니다.", "S_HUBTOHUB_SEARCH_ONE" )
-	,;
+	HUBTOHUB_SEARCH_ONE(HttpStatus.OK, "단건 루트가 성공적으로 조회되었습니다.", "S_HUBTOHUB_SEARCH_ONE"),
+	HUB_CREATE(HttpStatus.CREATED, "허브가 성공적으로 생성되었습니다.", "S_HUB_CREATE"),
+	HUB_SEARCH(HttpStatus.OK, "허브가 성공적으로 조회되었습니다.", "S_HUB_SEARCH"),
+	;
 
 	private final HttpStatus statusCode;
 	private final String message;
