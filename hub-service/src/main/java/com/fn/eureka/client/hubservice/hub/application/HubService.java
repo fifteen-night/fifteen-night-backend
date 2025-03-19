b48529fd63af050adbd2fb50398f84a39dc75a85
Goldbar97 <ehrm1357@gmail.com>
2025-03-19 11:30:11 +0900

    #5 feat: hub - CommonResponse 적용

--- hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/application/HubService.java
+++ hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/application/HubService.java
@@ -2,20 +2,20 @@ package com.fn.eureka.client.hubservice.hub.application;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.fn.eureka.client.hubservice.hub.application.dto.request.CreateHubRequest;
 import com.fn.eureka.client.hubservice.hub.application.dto.request.UpdateHubRequest;
 import com.fn.eureka.client.hubservice.hub.application.dto.response.CreateHubResponse;
 import com.fn.eureka.client.hubservice.hub.application.dto.response.ReadHubResponse;
-import com.fn.eureka.client.hubservice.hub.application.dto.response.SearchHubResponse;
 
 public interface HubService {
 	CreateHubResponse createHub(CreateHubRequest request);
 
 	ReadHubResponse readHub(UUID hubId);
 
-	SearchHubResponse searchHub(Pageable pageable, String hubName);
+	Page<ReadHubResponse> searchHub(Pageable pageable, String hubName);
 
 	void updateHub(UUID hubId, UpdateHubRequest request);
 
