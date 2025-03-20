38a824322b5851ac77d1ea72ac5820114a3af6f2
STW5 <xodnd8384@gmail.com>
2025-03-20 15:50:58 +0900

    #67 feat: slack - 메시지 수정 기능 구현

--- common/src/main/java/com/fn/common/global/success/SuccessCode.java
+++ common/src/main/java/com/fn/common/global/success/SuccessCode.java
@@ -39,8 +39,10 @@ public enum SuccessCode {
 	// Slack 관련 성공 응답
 	SLACK_MESSAGE_SENT(HttpStatus.CREATED, "Slack 메시지가 성공적으로 전송되었습니다.", "S_SLACK_MESSAGE_SENT"),
 	SLACK_MESSAGE_FOUND(HttpStatus.OK, "Slack 메시지가 성공적으로 조회되었습니다.", "S_SLACK_MESSAGE_FOUND"),
-	SLACK_MESSAGE_LIST_FOUND(HttpStatus.OK, "Slack 메시지 목록이 성공적으로 조회되었습니다.", "S_SLACK_MESSAGE_LIST_FOUND")
+	SLACK_MESSAGE_LIST_FOUND(HttpStatus.OK, "Slack 메시지 목록이 성공적으로 조회되었습니다.", "S_SLACK_MESSAGE_LIST_FOUND"),
+	SLACK_MESSAGE_UPDATED(HttpStatus.OK, "Slack 메시지가 성공적으로 수정되었습니다.", "S_SLACK_MESSAGE_UPDATED")
 	;
+
 	private final HttpStatus statusCode;
 	private final String message;
 	private final String code;
