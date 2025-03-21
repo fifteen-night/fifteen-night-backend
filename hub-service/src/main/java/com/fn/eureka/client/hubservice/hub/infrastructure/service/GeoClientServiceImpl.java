6f8c1d0104a2885634ac7d1de5b01a75adff0ca5
Goldbar97 <ehrm1357@gmail.com>
2025-03-21 22:39:50 +0900

    #74 refactor: hub - 디렉토리 및 이름 변경

--- hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/infrastructure/service/GeoClientServiceImpl.java
+++ hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/infrastructure/service/GeoClientServiceImpl.java
@@ -1,25 +1,28 @@
-package com.fn.eureka.client.hubservice.hub.infrastructure;
+package com.fn.eureka.client.hubservice.hub.infrastructure.service;
 
 import java.math.BigDecimal;
 
 import org.springframework.stereotype.Service;
 
-import com.fn.eureka.client.hubservice.hub.application.GeoService;
+import com.fn.common.global.exception.CustomApiException;
+import com.fn.eureka.client.hubservice.hub.application.GeoClientService;
 import com.fn.eureka.client.hubservice.hub.application.dto.response.GeoResponse;
 import com.fn.eureka.client.hubservice.hub.application.dto.response.Point;
+import com.fn.eureka.client.hubservice.hub.exception.HubException;
+import com.fn.eureka.client.hubservice.hub.infrastructure.client.GeoClient;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class GeoServiceImpl implements GeoService {
+public class GeoClientServiceImpl implements GeoClientService {
 	private final GeoClient geoClient;
 
 	public Point getPoint(String address) {
 		GeoResponse response = geoClient.getPoint(address);
 
 		if (response.getAddresses() == null || response.getAddresses().isEmpty()) {
-			throw new RuntimeException("없는 주소입니다.");
+			throw new CustomApiException(HubException.HUB_ADDRESS_NOT_FOUND);
 		}
 
 		GeoResponse.Address result = response.getAddresses().get(0);
--- hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/infrastructure/service/HubServiceImpl.java
+++ hub-service/src/main/java/com/fn/eureka/client/hubservice/hub/infrastructure/service/HubServiceImpl.java
@@ -1,4 +1,4 @@
-package com.fn.eureka.client.hubservice.hub.infrastructure;
+package com.fn.eureka.client.hubservice.hub.infrastructure.service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -9,23 +9,29 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.fn.common.global.exception.NotFoundException;
-import com.fn.eureka.client.hubservice.hub.application.GeoService;
+import com.fn.common.global.exception.CustomApiException;
+import com.fn.eureka.client.hubservice.hub.application.GeoClientService;
 import com.fn.eureka.client.hubservice.hub.application.HubService;
+import com.fn.eureka.client.hubservice.hub.application.ProductClientService;
+import com.fn.eureka.client.hubservice.hub.application.UserClientService;
 import com.fn.eureka.client.hubservice.hub.application.dto.mapper.HubMapper;
+import com.fn.eureka.client.hubservice.hub.application.dto.request.CheckHubManagerRequest;
 import com.fn.eureka.client.hubservice.hub.application.dto.request.CreateHubRequest;
 import com.fn.eureka.client.hubservice.hub.application.dto.request.UpdateHubRequest;
+import com.fn.eureka.client.hubservice.hub.application.dto.response.CheckHubManagerResponse;
 import com.fn.eureka.client.hubservice.hub.application.dto.response.CreateHubResponse;
 import com.fn.eureka.client.hubservice.hub.application.dto.response.Point;
 import com.fn.eureka.client.hubservice.hub.application.dto.response.ReadHubResponse;
 import com.fn.eureka.client.hubservice.hub.domain.Hub;
 import com.fn.eureka.client.hubservice.hub.domain.repository.HubRepository;
+import com.fn.eureka.client.hubservice.hub.exception.HubException;
 import com.fn.eureka.client.hubservice.hub_stock.application.dto.mapper.HubStockMapper;
 import com.fn.eureka.client.hubservice.hub_stock.application.dto.request.CreateHubStockRequest;
 import com.fn.eureka.client.hubservice.hub_stock.application.dto.request.UpdateHubStockRequest;
 import com.fn.eureka.client.hubservice.hub_stock.application.dto.response.CreateHubStockResponse;
 import com.fn.eureka.client.hubservice.hub_stock.application.dto.response.ReadHubStockResponse;
 import com.fn.eureka.client.hubservice.hub_stock.domain.HubStock;
+import com.fn.eureka.client.hubservice.hub_stock.exception.HubStockException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -33,13 +39,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class HubServiceImpl implements HubService {
 	private final HubRepository hubRepository;
-	private final GeoService geoService;
+	private final GeoClientService geoClientService;
+	private final UserClientService userClientService;
+	private final ProductClientService productClientService;
 
 	// 허브 관련 시작
 	@Override
 	@Transactional
 	public CreateHubResponse createHub(CreateHubRequest request) {
-		Point point = geoService.getPoint(request.getHubAddress());
+		Point point = geoClientService.getPoint(request.getHubAddress());
+
+		if (!userClientService.checkUserIfManager(request.getHubManagerId())) {
+			throw new CustomApiException(HubException.USER_NOT_QUALIFIED);
+		}
+
+		if (hubRepository.existsByHubAddress(request.getHubAddress())) {
+			throw new CustomApiException(HubException.HUB_ALREADY_EXISTS);
+		}
+
 		Hub hub = hubRepository.save(HubMapper.toEntity(request, point));
 
 		return HubMapper.toDto(hub);
@@ -48,7 +65,7 @@ public class HubServiceImpl implements HubService {
 	@Override
 	@Transactional(readOnly = true)
 	public ReadHubResponse readHub(UUID hubId) {
-		Hub hub = hubRepository.findById(hubId).orElseThrow(() -> new NotFoundException("없는 허브입니다."));
+		Hub hub = findHubById(hubId);
 
 		return HubMapper.toDto(hub, hubId);
 	}
@@ -62,7 +79,7 @@ public class HubServiceImpl implements HubService {
 	@Override
 	@Transactional
 	public void updateHub(UUID hubId, UpdateHubRequest request) {
-		Hub hub = hubRepository.findById(hubId).orElseThrow(() -> new NotFoundException("없는 허브입니다."));
+		Hub hub = findHubById(hubId);
 
 		if (request.getHubName() != null) {
 			hub.updateHubName(request.getHubName());
@@ -80,14 +97,21 @@ public class HubServiceImpl implements HubService {
 	@Override
 	@Transactional
 	public void deleteHub(UUID hubId) {
-		Hub hub = hubRepository.findById(hubId).orElseThrow(() -> new NotFoundException("없는 허브입니다."));
+		Hub hub = findHubById(hubId);
 
 		hub.markAsDeleted();
 	}
 
 	@Override
 	public Hub findHubById(UUID id) {
-		return hubRepository.findById(id).orElseThrow(() -> new NotFoundException("없는 허브입니다."));
+		return hubRepository.findById(id).orElseThrow(() -> new CustomApiException(HubException.HUB_NOT_FOUND));
+	}
+
+	@Override
+	public CheckHubManagerResponse checkHubManager(CheckHubManagerRequest request) {
+		Hub hub = findHubById(request.getHubId());
+
+		return new CheckHubManagerResponse(hub.getHubManagerId().equals(request.getUserId()));
 	}
 	// 허브 관련 끝
 
@@ -96,6 +120,10 @@ public class HubServiceImpl implements HubService {
 	@Transactional
 	public CreateHubStockResponse createHubStock(UUID hubId, CreateHubStockRequest request) {
 		Hub hub = findHubById(hubId);
+
+		if (!productClientService.checkProductIfPresent(request.getProductId())) {
+			throw new CustomApiException(HubException.PRODUCT_NOT_FOUND);
+		}
 
 		Optional<HubStock> optionalHubStock = hubRepository.findHubStockByHubIdAndProductId(hubId,
 			request.getProductId());
@@ -133,6 +161,10 @@ public class HubServiceImpl implements HubService {
 	public void updateHubStock(UUID hubId, UUID productId, UpdateHubStockRequest request) {
 		HubStock hubStock = findHubStockByHubIdAndProductId(hubId, productId);
 
+		if (hubStock.getHsQuantity() - request.getQuantity() < 0) {
+			throw new CustomApiException(HubException.HUB_STOCK_LESS_QUANTITY);
+		}
+
 		hubStock.updateQuantity(request.getQuantity());
 	}
 
@@ -147,6 +179,6 @@ public class HubServiceImpl implements HubService {
 	@Override
 	public HubStock findHubStockByHubIdAndProductId(UUID hubId, UUID productId) {
 		return hubRepository.findHubStockByHubIdAndProductId(hubId, productId)
-			.orElseThrow(() -> new NotFoundException("없는 재고입니다."));
+			.orElseThrow(() -> new CustomApiException(HubStockException.HUB_STOCK_NOT_FOUND));
 	}
 }
