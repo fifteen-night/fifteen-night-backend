11ec77a3a55547586a65a065306d453a886bc2db
Goldbar97 <ehrm1357@gmail.com>
2025-03-21 20:55:46 +0900

    #74 feat: hub - SuccessCode 추가

--- common/src/main/java/com/fn/common/global/success/SuccessCode.java
+++ common/src/main/java/com/fn/common/global/success/SuccessCode.java
@@ -8,21 +8,23 @@ import lombok.Getter;
 public enum SuccessCode {
 
 	// 여기서 공통 성공 응답 생성
-    // HubToHub 관련 성공응답
+
+	// HubToHub 관련 성공응답
 	HUBTOHUB_CREATE(HttpStatus.CREATED, "루트가 성공적으로 생성되었습니다.", "S_HUBTOHUB_CREATE"),
-	HUBTOHUB_SEARCH_ALL(HttpStatus.OK, "모든 루트가 성공적으로 조회되었습니다.","S_HUBTOHUB_SEARCH_ALL" ),
+	HUBTOHUB_SEARCH_ALL(HttpStatus.OK, "모든 루트가 성공적으로 조회되었습니다.", "S_HUBTOHUB_SEARCH_ALL"),
 	HUBTOHUB_SEARCH_ONE(HttpStatus.OK, "단건 루트가 성공적으로 조회되었습니다.", "S_HUBTOHUB_SEARCH_ONE"),
-	HUBTOHUB_SOFT_DELETE(HttpStatus.NO_CONTENT, "해당 루트가 삭제되었습니다.", "S_HUBTOHUB_SOFT_DELETE" ),
-	HUBTOHUB_UPDATE(HttpStatus.OK, "루트가 성공적으로 수정되었습니다.","S_HUBTOHUB_UPDATE" ),
+	HUBTOHUB_SOFT_DELETE(HttpStatus.NO_CONTENT, "해당 루트가 삭제되었습니다.", "S_HUBTOHUB_SOFT_DELETE"),
+	HUBTOHUB_UPDATE(HttpStatus.OK, "루트가 성공적으로 수정되었습니다.", "S_HUBTOHUB_UPDATE"),
 
 	// Delivery 관련 성공응답
-	DELIVERY_CREATE(HttpStatus.CREATED, "배송이 성공적으로 생성되었습니다.", "S_DELIVERY_CREATE" ),
-	DELIVERY_SEARCH_ONE(HttpStatus.OK, "단건 배송이 성공적으로 조회되었습니다.","S_DELIVERY_SEARCH_ONE" ),
-	DELIVERY_SEARCH_ALL(HttpStatus.OK, "모든 배송이 성공적으로 조회되었습니다.","S_DELIVERY_SEARCH_ALL" ),
+	DELIVERY_CREATE(HttpStatus.CREATED, "배송이 성공적으로 생성되었습니다.", "S_DELIVERY_CREATE"),
+	DELIVERY_SEARCH_ONE(HttpStatus.OK, "단건 배송이 성공적으로 조회되었습니다.", "S_DELIVERY_SEARCH_ONE"),
+	DELIVERY_SEARCH_ALL(HttpStatus.OK, "모든 배송이 성공적으로 조회되었습니다.", "S_DELIVERY_SEARCH_ALL"),
 
 	// Hub 관련 성공응답
 	HUB_CREATE(HttpStatus.CREATED, "허브가 성공적으로 생성되었습니다.", "S_HUB_CREATE"),
 	HUB_SEARCH(HttpStatus.OK, "허브가 성공적으로 조회되었습니다.", "S_HUB_SEARCH"),
+	HUB_MANAGER_CHECK(HttpStatus.OK, "허브 매니저가 확인되었습니다.", "S_HUB_MANAGER_CHECK"),
 
 	// HubStock 관련 성공 응답
 	HUB_STOCK_CREATE(HttpStatus.CREATED, "허브 재고가 성공적으로 생성되었습니다.", "S_HUB_STOCK_CREATE"),
@@ -42,7 +44,7 @@ public enum SuccessCode {
 	SLACK_MESSAGE_SENT(HttpStatus.CREATED, "Slack 메시지가 성공적으로 전송되었습니다.", "S_SLACK_MESSAGE_SENT"),
 	SLACK_MESSAGE_FOUND(HttpStatus.OK, "Slack 메시지가 성공적으로 조회되었습니다.", "S_SLACK_MESSAGE_FOUND"),
 	SLACK_MESSAGE_LIST_FOUND(HttpStatus.OK, "Slack 메시지 목록이 성공적으로 조회되었습니다.", "S_SLACK_MESSAGE_LIST_FOUND"),
-	SLACK_MESSAGE_UPDATED(HttpStatus.OK, "Slack 메시지가 성공적으로 수정되었습니다.", "S_SLACK_MESSAGE_UPDATED")
+	SLACK_MESSAGE_UPDATED(HttpStatus.OK, "Slack 메시지가 성공적으로 수정되었습니다.", "S_SLACK_MESSAGE_UPDATED"),
 	;
 
 	private final HttpStatus statusCode;
