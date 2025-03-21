8cd6dc65668591e03681b227c2a5b027dab59893
김재현 <94097685+iconew123@users.noreply.github.com>
2025-03-21 15:16:13 +0900

    #79 feat: delivery-service DeliveryRoute 엔티티설계
    - DeliveryRoute 엔티티설계
    - DeliveryRoute의 Sequence 정규화 엔티티 설계(OneToMany)

---
+++ delivery-service/src/main/java/com/fn/eureka/client/deliveryservice/domain/model/deliveryRouteSequence/DeliveryRouteSequence.java
@@ -0,0 +1,31 @@
+package com.fn.eureka.client.deliveryservice.domain.model.deliveryRouteSequence;
+
+import java.util.UUID;
+
+import com.fn.common.global.BaseEntity;
+import com.fn.eureka.client.deliveryservice.domain.model.deliveryRoute.DeliveryRoute;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "p_delivery_route_sequence", schema = "delivery")
+@Getter
+@NoArgsConstructor
+public class DeliveryRouteSequence extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID sequenceId;
+
+	@ManyToOne
+	@JoinColumn(name = "deliveryRouteId", nullable = false)
+	private DeliveryRoute deliveryRoute;
+}
