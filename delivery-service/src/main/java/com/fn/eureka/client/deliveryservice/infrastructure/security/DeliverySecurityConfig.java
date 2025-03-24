9ad7d294fee0f4951320523fc4a5c55bcb4ec571
김재현 <94097685+iconew123@users.noreply.github.com>
2025-03-24 12:28:13 +0900

    #104 refactor: delivery-service PreAuthorize 유저 역할 검증
    - @PreAuthorize 유저 역할 검증
    - 오타 수정

--- delivery-service/src/main/java/com/fn/eureka/client/deliveryservice/infrastructure/security/DeliverySecurityConfig.java
+++ delivery-service/src/main/java/com/fn/eureka/client/deliveryservice/infrastructure/security/DeliverySecurityConfig.java
@@ -2,6 +2,8 @@ package com.fn.eureka.client.deliveryservice.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 public class DeliverySecurityConfig {
 
